e8671181c3330a8758f401c10f3de9a2893157b6
Muhammad Ahmed Cheema <mcheema@fandm.edu>
2023-02-04 20:50:10 -0800

    [Web-5] Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
-  post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'general#index'
 end
